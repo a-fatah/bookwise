09692579a8a6933afce68ca79a08b7352b6689bd
a-fatah <abdulfatah66@gmail.com>
2023-02-21 01:57:59 +0100

    add /books route

---
+++ src/main/scala/io/freevariable/bookwise/Server.scala
@@ -0,0 +1,55 @@
+package io.freevariable.bookwise
+
+import cats.effect._
+import com.comcast.ip4s._
+import io.circe.generic.auto._
+import io.circe.syntax._
+import org.http4s._
+import org.http4s.circe._
+import org.http4s.dsl.io._
+import org.http4s.ember.server._
+import org.http4s.implicits._
+
+
+object Server extends IOApp {
+
+  case class Book(title: String, author: Author, pages: Int)
+  case class Author(name: String)
+
+  val books = List(
+    Book("To Kill a Mockingbird", Author("Harper Lee"), 281),
+    Book("The Great Gatsby", Author("F. Scott Fitzgerald"), 180),
+    Book("Pride and Prejudice", Author("Jane Austen"), 279),
+    Book("1984", Author("George Orwell"), 328),
+    Book("The Catcher in the Rye", Author("J.D. Salinger"), 234),
+    Book("One Hundred Years of Solitude", Author("Gabriel Garcia Marquez"), 417),
+    Book("Brave New World", Author("Aldous Huxley"), 288),
+    Book("The Hobbit", Author("J.R.R. Tolkien"), 304),
+    Book("To the Lighthouse", Author("Virginia Woolf"), 209),
+    Book("The Picture of Dorian Gray", Author("Oscar Wilde"), 254)
+  )
+
+  def run(args: List[String]): IO[ExitCode] = {
+
+    val bookRoutes = HttpRoutes.of[IO] {
+      case GET -> Root / "books" =>
+        Ok(books.asJson)
+      case GET -> Root / "books" / IntVar(id) =>
+        books.lift(id) match {
+          case Some(book) => Ok(book.asJson)
+          case None => NotFound()
+        }
+    }
+
+    val app = bookRoutes.orNotFound
+
+    val server = EmberServerBuilder
+      .default[IO]
+      .withHost(ipv4"0.0.0.0")
+      .withPort(port"8080")
+      .withHttpApp(app)
+      .build
+
+    server.use(_ => IO.never).as(ExitCode.Success)
+  }
+}
