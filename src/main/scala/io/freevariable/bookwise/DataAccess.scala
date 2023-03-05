108f293547c9683ebc9981d44327651e48c52141
a-fatah <abdulfatah66@gmail.com>
2023-03-05 22:13:31 +0100

    implement book creation

--- src/main/scala/io/freevariable/bookwise/DataAccess.scala
+++ src/main/scala/io/freevariable/bookwise/DataAccess.scala
@@ -6,6 +6,8 @@ import liquibase.database.jvm.JdbcConnection
 import liquibase.resource.ClassLoaderResourceAccessor
 import slick.jdbc.JdbcProfile
 
+import scala.concurrent.ExecutionContext
+
 
 trait DatabaseProvider {
   val profile: JdbcProfile
@@ -48,6 +50,8 @@ trait BooksSchema { this: DatabaseProvider =>
   trait BookRepository {
     def all(): IO[Seq[(BookEntity, AuthorEntity)]]
     def get(id: Long): IO[Option[(BookEntity, AuthorEntity)]]
+
+    def save(book: BookEntity, author: AuthorEntity)(implicit ec: ExecutionContext): IO[(AuthorEntity, BookEntity)]
   }
 
   class BookRepositoryImpl extends BookRepository {
@@ -58,6 +62,15 @@ trait BooksSchema { this: DatabaseProvider =>
 
     override def all(): IO[Seq[(BookEntity, AuthorEntity)]] = {
       val result = books.join(authors).on(_.authorId === _.id).result
+      IO.fromFuture(IO(db.run(result)))
+    }
+
+    override def save(book: BookEntity, author: AuthorEntity)(implicit ec: ExecutionContext): IO[(AuthorEntity, BookEntity)] = {
+      val result = for {
+        a <- (authors returning authors.map(_.id) into ((author, id) => author.copy(id = Some(id)))) += author
+        b <- (books returning books.map(_.id) into ((book, id) => book.copy(id = Some(id)))) += book.copy(authorId = a.id.get)
+      } yield (a, b)
+
       IO.fromFuture(IO(db.run(result)))
     }
   }
@@ -73,6 +86,12 @@ class BookService {
 
   def get(id: Long): IO[Option[Book]] = repository.get(id) flatMap { opt =>
     IO(opt.map { case (book, author) => Book(book.title, Author(author.name), book.pages) })
+  }
+
+  def create(book: Book)(implicit ec: ExecutionContext): IO[Book] = {
+    repository.save(BookEntity(None, book.title, 0, book.pages), AuthorEntity(None, book.author.name)).map {
+      case (author, book) => Book(book.title, Author(author.name), book.pages)
+    }
   }
 
 }
