f711beb513854c741071f3c67dc9ab4fa67642d9
a-fatah <abdulfatah66@gmail.com>
2023-03-08 04:54:13 +0100

    refactor BookService

--- src/main/scala/io/freevariable/bookwise/BookService.scala
+++ src/main/scala/io/freevariable/bookwise/BookService.scala
@@ -14,13 +14,9 @@ trait BookService {
 
 class BookServiceImpl(private val repository: BookRepository) extends BookService {
 
-  override def getAll: IO[Seq[Book]] = repository.all().map(_.map {
-    case (book, author, publisher) => mapToBook(book, author, publisher)
-  })
+  override def getAll: IO[Seq[Book]] = repository.all().map(_.map(mapToBook))
 
-  override def get(id: BookId): IO[Option[Book]] = repository.get(id.value).map(_.map {
-    case (book, author, publisher) => mapToBook(book, author, publisher)
-  })
+  override def get(id: BookId): IO[Option[Book]] = repository.get(id.value).map(_.map(mapToBook))
 
   override def create(book: Book)(implicit ec: ExecutionContext): IO[(BookId, Book)] = {
     val bookEntity = BookEntity(None, book.title, book.isbn, 0, 0, book.pages)
@@ -28,17 +24,15 @@ class BookServiceImpl(private val repository: BookRepository) extends BookServic
     val publisherEntity = PublisherEntity(None, book.publisher.name)
 
     repository.save(bookEntity, authorEntity, publisherEntity).map {
-      case (author, book, publisher) => (BookId(book.id.get), mapToBook(book, author, publisher))
+      case (author, book, publisher) => (BookId(book.id.get), mapToBook((book, author, publisher)))
     }
   }
 
-  override def getByTitle(title: String): IO[Option[Book]] = {
-    repository.getByTitle(title).map(_.map {
-      case (book, author, publisher) => mapToBook(book, author, publisher)
-    })
-  }
+  override def getByTitle(title: String): IO[Option[Book]] =
+    repository.getByTitle(title).map(_.map(mapToBook))
 
-  private def mapToBook(book: BookEntity, author: AuthorEntity, publisher: PublisherEntity): Book = {
-    Book(book.title, book.isbn, Author(author.name), Publisher(publisher.name), book.pages)
+
+  private def mapToBook(tuple: (BookEntity, AuthorEntity, PublisherEntity)): Book = tuple match {
+    case (book, author, publisher) => Book(book.title, book.isbn, Author(author.name), Publisher(publisher.name), book.pages)
   }
 }
