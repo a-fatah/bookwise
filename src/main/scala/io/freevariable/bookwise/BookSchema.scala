7f26dee370f83cc5bb4f4c7cc9675a9dec6576ab
a-fatah <abdulfatah66@gmail.com>
2023-03-08 04:05:00 +0100

    define foreign key in books table

--- src/main/scala/io/freevariable/bookwise/BookSchema.scala
+++ src/main/scala/io/freevariable/bookwise/BookSchema.scala
@@ -10,20 +10,28 @@ trait BooksSchema { this: DatabaseProvider =>
 
   import profile.api._
 
+  class AuthorsTable(tag: Tag) extends Table[AuthorEntity](tag, "authors") {
+    def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+
+    def name = column[String]("name")
+
+    def * = (id.?, name) <> (AuthorEntity.tupled, AuthorEntity.unapply)
+  }
+
+  val authors = TableQuery[AuthorsTable]
+
   class BooksTable (tag: Tag) extends Table[BookEntity](tag, "books") {
     def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
     def title = column[String]("title")
     def authorId = column[Long]("author_id")
     def pages = column[Int]("pages")
 
+    def author = foreignKey("author_fk", authorId, authors)(_.id)
+
     def * = (id.?, title, authorId, pages) <> (BookEntity.tupled, BookEntity.unapply)
   }
 
-  class AuthorsTable(tag: Tag) extends Table[AuthorEntity](tag, "authors") {
-    def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
-    def name = column[String]("name")
-    def * = (id.?, name) <> (AuthorEntity.tupled, AuthorEntity.unapply)
-  }
+
 
   def runMigrations(): IO[Unit] = {
     val liquibase = new Liquibase("db/changelog/changelog-master.xml",
